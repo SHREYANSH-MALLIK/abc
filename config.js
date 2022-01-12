de56f17cf11380cb800c6d7f72cc5a924e02d3b6
SHREYANSH-MALLIK <86109327+SHREYANSH-MALLIK@users.noreply.github.com>
2022-01-12 18:55:46 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB38PK55LmHdoLyVIuntlNG4t0Rx2E6hc8",
+    authDomain: "story-telling-app-51fe3.firebaseapp.com",
+    databaseURL: "https://story-telling-app-51fe3-default-rtdb.firebaseio.com",
+    projectId: "story-telling-app-51fe3",
+    storageBucket: "story-telling-app-51fe3.appspot.com",
+    messagingSenderId: "257470809792",
+    appId: "1:257470809792:web:8c82428cd24b6a029b8f25"
+  };
